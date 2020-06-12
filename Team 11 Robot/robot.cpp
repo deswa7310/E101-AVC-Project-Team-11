40c03cf64969c762c01b19db9873ef953f23ea2b
Jayman874 <66066183+Jayman874@users.noreply.github.com>
2020-06-12 15:46:36 +1200

    Update robot.cpp
    Added base code for core. Error calculation is not working yet

--- Team 11 Robot/robot.cpp
+++ Team 11 Robot/robot.cpp
@@ -1,18 +1,39 @@
-
-#include "robot.hpp"
-int main(){
-	if (initClientRobot() !=0){
-		std::cout<<" Error initializing robot"<<std::endl;
-	}
-    double vLeft = 30.0;
-    double vRight = 30.0;
+#include "robot.hpp" 
+ 
+int main(){ 
+    cameraView.width = 100;     
+    cameraView.height = 100; 
+    cameraView.data = new char[cameraView.width * cameraView.height*3]; 
+    cameraView.n_bytes = cameraView.width * cameraView.height*3; 
+     
+    if (connectNetwork()!=0){ 
+      return -1; 
+   } 
+    double vLeft = 0.0; 
+    double vRight = 0.0; 
     
-    takePicture();
-    SavePPMFile("i0.ppm",cameraView);
-    while(1){
-      setMotors(vLeft,vRight);   
-      std::cout<<" vLeft="<<vLeft<<"  vRight="<<vRight<<std::endl;
-       usleep(10000);
-  } //while
+    while(1){ // runs forever 
+      takePicture(); 
+      double error = 0.0;
+      unsigned char v_go = 10;
+      unsigned char dv; 
+      for (int i =0; i<150; i++){
+		  int pix = get_pixel(cameraView, 50, i, 3);
+		  int isWhite;
+		  if (pix > 250){isWhite = 1;
+			  error = error + i-79.5;
+			  } else {isWhite = 0;}
+		  std::cout<<isWhite<< " ";
+	  }
+	  std::cout<<std::endl;
+	  dv = 5*error;
+	  vLeft = v_go - dv;
+	  vRight = v_go + dv;
+      setMotors(vLeft,vRight); 
+      std::cout<<" vLeft="<<vLeft<<" vRight="<<vRight<<std::endl;
+      usleep(10000);   
+      
+  } //while 
+ 
+} // main 
 
-} // main
