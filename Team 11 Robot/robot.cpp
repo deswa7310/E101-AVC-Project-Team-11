dc4e05ff8fabcfa5acddb7c8c4377ed40b161fd8
deswa7310 <66063484+deswa7310@users.noreply.github.com>
2020-06-12 21:21:05 +1200

    Update robot.cpp
    Updated it to fix the error issue. Also optimized it a bit. It should work now. Still don't know the purpose of the cameraView stuff at the top though haha.

--- Team 11 Robot/robot.cpp
+++ Team 11 Robot/robot.cpp
@@ -1,7 +1,7 @@
 #include "robot.hpp" 
  
 int main(){ 
-    cameraView.width = 100;     
+    cameraView.width = 150;     
     cameraView.height = 100; 
     cameraView.data = new char[cameraView.width * cameraView.height*3]; 
     cameraView.n_bytes = cameraView.width * cameraView.height*3; 
@@ -9,26 +9,29 @@ int main(){
     if (connectNetwork()!=0){ 
       return -1; 
    } 
-    double vLeft = 0.0; 
-    double vRight = 0.0; 
+    double vGo = 30.0;
+    double vRight, vLeft;
+    int image_width = 150;
+    double image_centre = (image_width-1)/2.0; // centre of width
     
     while(1){ // runs forever 
       takePicture(); 
-      double error = 0.0;
-      unsigned char v_go = 10;
-      unsigned char dv; 
-      for (int i =0; i<100; i++){// loops through all pixels rows in camera
+      int total_index = 0, white_pix_count = 0;
+      double average_index = 0.0; // the mean index
+	    
+      for (int i = 0; i < 150; i++){// loops through all pixels rows in camera
 		  int pix = get_pixel(cameraView, 50, i, 3);// gets all pixels in view of the camera
-		  int isWhite;
-		  if (pix > 250){isWhite = 1;// If pixel value is greater than 250 replace value with one
-			  error = error + i-79.5;// error for seeing how far white line is from center of camera //NOT CURRENTLY WORKING AS INTENDED
-			  } else {isWhite = 0;} // replace other pixel values with zero
-		  std::cout<<isWhite<< " ";
+		  if (pix > 250){ // if pixel is white
+			  total_index += 1; // counts number of indices
+			  white_pix_count++;
+		  }
 	  }
-	  std::cout<<std::endl;
-	  dv = 5*error;// depending on error value turns wheel with a direction and speed centering white line in camera
-	  vLeft = v_go - dv;// calculation to see how fast wheels should move 
-	  vRight = v_go + dv;
+	  average_index = total_index / white_pix_count; // finds central index
+	  double error = average_index - image_centre; // finds distance from central index to centre of image
+	  
+	  double dv = 0.5*error; // depending on error value turns wheel with a direction and speed centering white line in camera
+	  vLeft = v_go + dv; // calculation to see how fast wheels should move 
+	  vRight = v_go - dv;
       setMotors(vLeft,vRight); //set motor speed 
       std::cout<<" vLeft="<<vLeft<<" vRight="<<vRight<<std::endl;
       usleep(10000);   
